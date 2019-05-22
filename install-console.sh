8ecae1e5d1aec0bf8a8e7681bc25db5147868125
Sean Glover <sean@seanglover.com>
2019-05-22 16:12:49 -0400

    Fix console install

--- install-console.sh
+++ install-console.sh
@@ -8,5 +8,6 @@ cd ~/source/console-charts/enterprise-suite
 kubectl create namespace lightbend-console
 kubectl -n lightbend-console create secret docker-registry commercial-credentials --docker-server=lightbend-docker-commercial-registry.bintray.io --docker-username=${BINTRAY_USER} --docker-password=${BINTRAY_PWD}
 helm install --name=lightbend-console --namespace=lightbend-console -f scripts/pipelines.yaml .
-create route
-kubectl apply -f console-ingress.yaml -n lightbend-console
+# create route
+cd ~/source/kafka-in-jail
+kubectl apply -f ./resources/console-ingress.yaml -n lightbend-console
