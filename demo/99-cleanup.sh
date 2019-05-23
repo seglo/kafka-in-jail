5db6b66e44c171f12fe67b2af0c8854cc25beea8
Sean Glover <sean@seanglover.com>
2019-05-23 10:13:40 -0400

    Reorder cleanup

--- demo/99-cleanup.sh
+++ demo/99-cleanup.sh
@@ -1,11 +1,13 @@
-# delete cluster
-kubectl delete kafka simple-strimzi -n strimzi
+# delete perf-producer deployment
+kubectl delete deployment producer -n strimzi
 
 # delete topic
 kubectl delete kafkatopic simple-topic -n strimzi
 
+# delete cluster
+kubectl delete kafka simple-strimzi -n strimzi
+
 # delete pvc's
 kubectl delete pvc -l strimzi.io/cluster=simple-strimzi -n strimzi
 
-# delete perf-producer deployment
-kubectl delete deployment producer -n strimzi
+
